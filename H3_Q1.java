85a51c350113cda988ad6dd5d3b9b93c64e42b27
Osama Abdu Ahmed Algumaei <94931636+OsamaAboAdel@users.noreply.github.com>
2022-03-07 21:58:02 +0300

    Add files via upload

---
+++ H3_Q1.java
@@ -0,0 +1,8 @@
+package Third_Lab;
+
+/**
+ * Created by ${*  Osama Abo Adel  *} on 2/7/2022.
+ */
+public class H3_Q1 {
+
+}
---
+++ Test.java
@@ -0,0 +1,59 @@
+//package Third_Lab;
+//
+//import java.util.Scanner;
+//
+///**
+// * Created by ${*  Osama Abo Adel  *} on 1/31/2022.
+// */
+//public class Test {
+//    public static void main(String[] args) {
+//        Scanner in=new Scanner(System.in);
+//        SingleLinkedList<String> numList=new SingleLinkedList<String>();
+//        for (int i = 0; i < 4; i++) {
+//            numList.addLast(in.nextLine());
+//        }
+////        for (int i = 0; i < 2; i++) {
+////            System.out.println(numList.toString());
+////        }
+//        for (int i = 0; i < 4; i++) {
+//            System.out.println(numList.removeFirst()+"\t");
+//        }
+//        System.out.println(numList.find());
+////        System.out.println("\n"+numList.first());
+////        System.out.println(numList.last());
+//    }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+////    public static void main(String[] args) {
+////        Scanner in = new  Scanner(System.in);
+////        SingleLinkedList<String> numList = new SingleLinkedList<String>();
+////        for (int i = 0; i < 4; i++) {
+////            numList.addFirst(in.nextLine());
+////        }
+//////        for (int i = 0; i < 4; i++) {
+//////            System.out.println(numList.removFirst());
+//////        }
+//////
+//////        numList.addLast("ss");
+//////        numList.addFirst("vv");
+////
+////        System.out.println("size of list: "+numList.size());
+////        System.out.println("First of list: "+numList.first());
+////        System.out.println("Last of list: "+numList.last());
+////
+////    }
+//}
